acca3add6417e276d1312adc153c9df56fce24dd
Gaurav Kaushik <gaurav.kaushik107@gmail.com>
2016-05-08 23:33:02 +0530

    download attachedImages into ProblemPostAdapter and display in recyclerView

--- app/src/main/java/com/nychareport/backlog/adapter/ProblemPostAdapter.java
+++ app/src/main/java/com/nychareport/backlog/adapter/ProblemPostAdapter.java
@@ -1,11 +1,18 @@
 package com.nychareport.backlog.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
 
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
 import com.nychareport.backlog.BackLogApplication;
 import com.nychareport.backlog.Constants;
 import com.nychareport.backlog.R;
-import com.nychareport.backlog.Utils;
+import com.nychareport.backlog.misc.AWSUtils;
+import com.nychareport.backlog.misc.Utils;
 import com.nychareport.backlog.models.Problem;
 import com.nychareport.backlog.viewholder.PostProblemViewHolder;
 
@@ -14,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -38,7 +46,7 @@ public class ProblemPostAdapter extends RecyclerView.Adapter<PostProblemViewHold
     }
 
     @Override
-    public void onBindViewHolder(PostProblemViewHolder holder, int position) {
+    public void onBindViewHolder(final PostProblemViewHolder holder, int position) {
         Problem problem = mDataSet.get(position);
         holder.problemTitle.setText(problem.getProblem());
         holder.problemDescription.setText(problem.getProblemDescription());
@@ -50,6 +58,29 @@ public class ProblemPostAdapter extends RecyclerView.Adapter<PostProblemViewHold
             holder.timeCreated.setVisibility(View.GONE);
         }
         holder.problemDescription.setText(problem.getProblemDescription());
+        final File file = new File(Environment.getExternalStorageDirectory().toString() + "/" + problem.getProblemPic());
+        // Initiate the download
+        TransferObserver observer = AWSUtils.getTransferUtility(BackLogApplication.getCurrentInstance())
+                .download(Constants.BUCKET_NAME, problem.getProblemPic(), file);
+        observer.setTransferListener(new TransferListener() {
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                if (state == TransferState.COMPLETED) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(file.getPath());
+                    holder.attachedProblemImage.setImageBitmap(bitmap);
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+
+            }
+        });
     }
 
     @Override
